dc357a206afb4338f557b1f6454ffc5904a080f2
Rashid Gaitov <rashidgaitov@gmail.com>
2022-07-31 16:53:16 +0300

    Initial Commit

---
+++ SquareSpace/SquareSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareSpaceApp.swift
+//  SquareSpace
+//
+//  Created by Rashid Gaitov on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SquareSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
