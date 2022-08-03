bfb1d3da9e2144ff45cb0a16532f06fea20c6b35
Rashid Gaitov <rashidgaitov@gmail.com>
2022-08-03 10:13:44 +0300

    Created List View

--- SquareSpace/VideoListView.swift
+++ SquareSpace/VideoListView.swift
@@ -9,19 +9,23 @@ import SwiftUI
 
 struct VideoListView: View {
     var body: some View {
-        HStack {
-            Image("37-tips")
-                .resizable()
-                .scaledToFit()
-                .frame(height: 80)
-                .cornerRadius(4)
-            VStack(alignment: .leading, spacing: 5) {
-                Text("YouTube Video 1 YouTube Video 1 YouTube Video 1")
-                    .font(.headline)
-                    .lineLimit(2)
-                Text("Video Description Video Description  Video Description ")
-                    .font(.subheadline)
-                    .lineLimit(2)
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            HStack {
+                Image("37-tips")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .cornerRadius(4)
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("YouTube Video 1")
+                        .font(.headline)
+                        .lineLimit(2)
+                        .minimumScaleFactor(0.5)
+                    Text("Video Description")
+                        .font(.subheadline)
+                        .lineLimit(2)
+                }
+                
             }
         }
     }
